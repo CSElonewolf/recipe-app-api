0289033af75ce91a5c0bec88169cc47fcacf354f
CSELonewolf <devkantgorai1@gmail.com>
2022-02-25 04:25:04 +0530

    Add serializers and views to generate token for the corresponding user credentials to enable authentication.

--- user/serializers.py
+++ user/serializers.py
@@ -1,6 +1,6 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model,authenticate
 from rest_framework import serializers
-
+from django.utils.translation import ugettext_lazy as _
 
 class UserSerializer(serializers.ModelSerializer):
 	"""Serializer for the users object """
@@ -20,3 +20,29 @@ class UserSerializer(serializers.ModelSerializer):
 		return get_user_model().objects.create_user(**validated_data)
 
 
+class AuthTokenSerializer(serializers.Serializer):
+	"""Serializer for the user authentication object"""
+	email = serializers.CharField()
+	password = serializers.CharField(
+		style={'input_type':'password'},
+		trim_whitespace=False
+	)
+
+	def validate(self,attrs):
+		"""Validate and authenticate the user"""
+		email = attrs.get('email')
+		password = attrs.get('password')
+
+
+		user = authenticate(
+			request =self.context.get('request'),
+			email = email,
+			password = password
+		)
+
+		if not user:
+			msg = ('Unable to authenicate with provided credentials')
+			raise serializers.ValidationError(msg,code='authentication')
+
+		attrs['user'] = user
+		return attrs
--- user/urls.py
+++ user/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('create/',views.CreateUserView.as_view(),name='create'),
+    path('token/',views.CreateTokenView.as_view(),name='token'),
 ]
