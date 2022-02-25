d67f8a9e78a39851a7fabea2e612deb13278a3cd
CSELonewolf <devkantgorai1@gmail.com>
2022-02-25 14:44:50 +0530

    Add feature to update and retrieve authenticated user details.

--- core/models.py
+++ core/models.py
@@ -29,7 +29,7 @@ class User(AbstractBaseUser,PermissionsMixin):
 	email = models.EmailField(max_length=255,unique=True)
 	name = models.CharField(max_length=255)
 	is_active =models.BooleanField(default=True)
-	is_staff = models.BooleanField(default=False)
+	is_staff = models.  BooleanField(default=False)
 
 
 	objects = UserManager()
--- recipe_app_api/settings.py
+++ recipe_app_api/settings.py
@@ -92,7 +92,6 @@ DATABASES = {
     }
 }
 
-print(DATABASES)
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
