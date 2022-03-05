345a2f536f05185515ef5551e4978a5edaa5ab3a
CSELonewolf <devkantgorai1@gmail.com>
2022-03-05 23:56:14 +0530

    Write unit test for testing behaviour of Recipe API during authenticated and unauthenticated situation.

--- recipe/tests/test_recipe_api.py
+++ recipe/tests/test_recipe_api.py
@@ -8,3 +8,75 @@ from rest_framework.test import APIClient
 
 from core.models import Recipe
 from recipe.serializers import RecipeSerializer
+
+RECIPES_URL = reverse('recipe-list')
+
+def sample_recipe(user,**params):
+	"""Create and return a smaple recipe"""
+	defaults ={
+		'title':'Sample User',
+		'time_minutes':10,
+		'price':5.00
+	}
+	defaults.update(params)
+
+	return Recipe.objects.create(user = user, **defaults)
+
+
+class PublicRecipeApiTests(TestCase):
+	"""Test unauthenticated recipe APi access"""
+
+	def setUp(self):
+		self.client = APIClient()
+
+	def test_auth_required(self):
+		"""Test that authentication is required"""
+		res = self.client.get(RECIPES_URL)
+		self.assertEqual(res.status_code,status.HTTP_403_FORBIDDEN)
+
+class PrivateRecipeApiTests(TestCase):
+	"""Test autheicated recipe API access"""
+
+	def setUp(self):
+		self.client = APIClient()
+		self.user = get_user_model().objects.create_user('test@gmail.com','testpass')
+
+		self.client.force_authenticate(self.user)
+
+	def test_retrieve_recipes(self):
+		"""Test retrieving a list of recipes """
+		sample_recipe(user = self.user)
+
+		res = self.client.get(RECIPES_URL)
+
+		recipes = Recipe.objects.all().order_by('-id')
+		serializer = RecipeSerializer(recipes,many=True)
+		self.assertEqual(res.status_code,status.HTTP_200_OK)
+		self.assertEqual(res.data,serializer.data)
+
+	def test_recipes_limited_to_user(self):
+		"""test retrieving recipes for user"""
+
+		user2 = get_user_model().objects.create_user(
+			'other@gmail.com',
+			'otherpass'
+		)
+
+		sample_recipe(user = user2)
+		sample_recipe(user =self.user)
+
+		res= self.client.get(RECIPES_URL)
+
+		recipes = Recipe.objects.filter(user = self.user)
+		serializer = RecipeSerializer(recipes,many = True)
+		self.assertEqual(res.status_code,status.HTTP_200_OK)
+		self.assertEqual(len(res.data),1)
+		self.assertEqual(res.data,serializer.data)
+
+
+
+
+
+
+
+
