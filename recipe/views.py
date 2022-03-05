6a2250817c74f5dc97accfb19d71bf68da4e3f5c
CSELonewolf <devkantgorai1@gmail.com>
2022-03-06 00:57:35 +0530

    Add RecipeDetail Serializer that displays details about ingredients and tags when a specific recipe is retrieved.

--- recipe/views.py
+++ recipe/views.py
@@ -41,4 +41,10 @@ class RecipeViewSet(viewsets.ModelViewSet):
 	def get_queryset(self):
 		return self.queryset.filter(user = self.request.user)
 
+	def get_serializer_class(self):
+		"""Return aappropiate serializer class"""
+		if self.action == 'retrieve':
+			return serializers.RecipeDetailSerializer
+
+		return self.serializer_class
 
